43716a81b326221e44d61be920a1d018b0215628
meekachm <meeka.chm@gmail.com>
2024-02-20 01:29:32 -0800

    Added EventType Class, Updated Models, and requirements

--- events_app/models.py
+++ events_app/models.py
@@ -1,27 +1,35 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+import enum
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(20), nullable=False)
+    # Add a relationship to the Event model
+    events = db.relationship("Event", secondary="guest_event_table", back_populates="guests")
 
-    events_attending = db.relationship('Event', secondary='guest_event_table', backref='guests')
+class EventType(enum.Enum):
+    ALL = "All"
+    PARTY = "Party"
+    STUDY = "Study"
+    NETWORKING = "Networking"
+    OTHER = "Other"
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     date_and_time = db.Column(db.DateTime, nullable=False)
-
-    guests = db.relationship('Guest', secondary='guest_event_table', backref='events')
-
-    # Stretch challenge
-    event_type = db.Column(db.Enum('Party', 'Study', 'Networking', name='event_types'))
+   
+    # Add an event_type column to the Event model
+    event_type = db.Column(db.Enum(EventType), default=EventType.ALL)
+    # Add a relationship to the Guest model
+    guests = db.relationship("Guest", secondary="guest_event_table", back_populates="events")
 
 guest_event_table = db.Table('guest_event_table',
-    db.Column('event_id', db.Integer, db.ForeignKey('events.id')),
-    db.Column('guest_id', db.Integer, db.ForeignKey('guests.id'))
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
 )
